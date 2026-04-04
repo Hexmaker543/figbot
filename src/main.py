d6072997fa89b0f1df09e898c6d4af2e3cbbd1d2
Hexmaker543 <HexMaker543@gmail.com>
2026-04-04 04:38:02 -0400

    Verify command made
    Need to make accept, deny, and close subcommands as well as ensure only 1 ticket open per person

--- src/main.py
+++ src/main.py
@@ -48,14 +48,32 @@ def load_config():
               "file exists and try again. \n")
         sys.exit()
 
+command_data = {}
+commands_filepath = 'commands.json'
+def load_commands():
+    try: 
+        with open(commands_filepath, 'r') as f: 
+            global command_data
+            command_data = json.load(f)
+            print('Commands Loaded\n')
+    except FileNotFoundError: 
+        print(f"Config file '{config_filepath}' could not be loaded, ensure"+
+              "file exists and try again. \n")
+        sys.exit()
+
 def save_config():
     with open(config_filepath, 'w') as f:
         json.dump(config, f, indent=4)
+
+def save_commands():
+    with open(commands_filepath, 'w') as f:
+        json.dump(command_data, f, indent=4)
 
 
 load_token()
 load_server_id()
 load_config()
+load_commands()
 
 
 # Client Class
@@ -66,7 +84,8 @@ class Client(commands.Bot):
 
         guild = self.get_guild(GUILD.id)
         await self.create_roles(guild=guild)
-        await self.sync_commands()
+
+        await send_embed()
 
     async def on_message(self, message):
         if message.author == self.user: return
@@ -153,9 +172,72 @@ class Verify_Button(discord.ui.View):
         self,
         interaction:discord.Interaction,
         button:discord.ui.Button):
-        if None in (interaction.guild, interaction.channel): return
+        if interaction.guild is None: return
 
-        ticket_category = config['commands']['verify']['category']
+        ticket_category = interaction.guild.get_channel(
+            command_data['verify']['category'])
+
+        if not isinstance(ticket_category, discord.CategoryChannel):
+            await interaction.response.send_message(
+                "'Ticket category' is not a category. Notify admin",
+                ephemeral=True)
+            return
+
+        channel_name=(
+                f'verification-ticket-{command_data['verify']['count']}')
+        verified_role = discord.utils.get(
+            interaction.guild.roles, name='Verified')
+
+        if verified_role is None:
+            await interaction.response.send_message(
+                "No verified role detected. Alert admin.",
+                ephemeral=True)
+            return
+
+        channel_overwrites= {
+            verified_role: discord.PermissionOverwrite(view_channel=False),
+            interaction.user: discord.PermissionOverwrite(
+                view_channel = True,
+                read_messages = True,
+                send_messages = True
+            )
+        }
+
+        await ticket_category.create_text_channel(
+            name=channel_name,
+            overwrites=channel_overwrites)
+        command_data['verify']['count'] += 1
+        save_commands()
+
+        ticket_channel = discord.utils.get(
+            interaction.guild.channels, name=channel_name)
+        if not isinstance(ticket_channel, discord.TextChannel):
+            await interaction.response.send_message(
+                    "Ticket channel could not be created, alert admin.",
+                ephemeral=True)
+            return
+
+        await ticket_channel.send(
+            f"<@{interaction.user.id}>\n"+
+            "# You will need to send:\n"+
+            "\n"
+            "**An image of you and your photo ID.** "+
+                "The only information you must leave "+
+                "visible is your face and birthday.\n"+
+            "\n"+
+            "**A paper with the name of the server on it and "+
+                "the current date.**\n"+
+            "\n"+
+            "**An image of you with the paper.**\n"+
+            "\n"+
+            "# Once you've sent them:\n"+
+            "\n"+
+            "@ the admins, one of them will come and verify you \n"+
+            "and your pictures will be deleted.")
+
+        await interaction.response.send_message(
+            f"Ticket opened in <#{ticket_channel.id}>",
+            ephemeral=True)
 
 
 intents = discord.Intents.default()
@@ -183,8 +265,9 @@ async def purge(
     await interaction.response.defer(ephemeral=True)
     deleted_count = len(await interaction.channel.purge(limit=message_count))
 
-    await interaction.followup.send(f'Deleted {deleted_count} messages',
-                                            ephemeral=True)
+    await interaction.followup.send(
+        f'Deleted {deleted_count} messages',
+        ephemeral=True)
 @purge.error
 async def purge_error(
     interaction: discord.Interaction, 
@@ -330,28 +413,7 @@ client.tree.add_command(verify_group, guild=GUILD)
 verify_command_permissions = {"send_messages": True}
 
     # embed
-@verify_group.command(
-    name='embed',
-    description='Send an embed with a verify button to the current channel')
-@app_commands.checks.has_permissions(
-        **verify_command_permissions,
-        administrator=True)
-async def set_embed(interaction:discord.Interaction):
-    await interaction.response.defer(ephemeral=True)
-
-    if interaction.guild is None:
-        await interaction.followup.send(
-        "This command cannot be used in DMs")
-        return
-
-    if not config['commands']['verify']['category']:
-        await interaction.followup.send(
-        "No ticket category set. Set one with '/verify category'.")
-        return
-
-    config['commands']['verify']['channel'] = interaction.channel_id
-    save_config()
-
+async def send_embed():
     embed = discord.Embed(
     title='Verification',
     description="You must verify your age to join the server",
@@ -371,10 +433,63 @@ async def set_embed(interaction:discord.Interaction):
         value='',
         inline=False)
 
-    await interaction.followup.send(view=Verify_Button(), embed=embed)
+    embed_channel = client.get_channel(
+        command_data['verify']['channel'])
+    if not isinstance(embed_channel, discord.TextChannel): 
+        print(f"embed_channel is type {type(embed_channel)}")
+        return
+
+    await embed_channel.purge()
+    await embed_channel.send(view=Verify_Button(), embed=embed)
+
+@verify_group.command(
+    name='embed',
+    description='Send an embed with a verify button to the current channel')
+@app_commands.checks.has_permissions(
+        **verify_command_permissions,
+        administrator=True)
+async def set_embed(interaction:discord.Interaction):
+
+    if interaction.guild is None:
+        await interaction.response.send_message(
+        "This command cannot be used in DMs",
+        ephemeral=True)
+        return
+
+    if not command_data['verify']['category']:
+        await interaction.response.send_message(
+            "No ticket category set. Set one with '/verify category'.",
+            ephemeral=True)
+        return
+
+    if not isinstance(interaction.channel, discord.TextChannel):
+        await interaction.response.send_message(
+            "This command can only be used in a text channel.")
+        return
+
+    if not command_data['verify']['channel']:
+        command_data['verify']['channel'] = interaction.channel.id
+        save_commands()
+
+    if not isinstance(interaction.channel, discord.TextChannel):
+        await interaction.response.send_message(
+            "This command must be used in a text channel.",
+            ephemeral=True)
+        return
+
+    await interaction.response.send_message("Embed sent.", ephemeral=True)
+    await send_embed()
 
     # category command
-async def set_category(interaction:discord.Interaction, category_name:int):
+@verify_group.command(
+    name='category',
+    description='Set a category to create verification tickets in')
+@app_commands.checks.has_permissions(
+    **verify_command_permissions,
+    administrator=True)
+@app_commands.describe(
+    category_name = 'Name of the category where tickets will be created')
+async def set_category(interaction:discord.Interaction, category_name:str):
     await interaction.response.defer(ephemeral=True)
 
     if interaction.guild is None:
@@ -384,13 +499,17 @@ async def set_category(interaction:discord.Interaction, category_name:int):
     category = discord.utils.get(
         interaction.guild.channels, 
         name=category_name)
+
     if category is None:
         await interaction.followup.send(
             f"'{category_name}' is not a valid category, ensure proper "+
             "spelling and try again")
         return
 
-    config['commands']['verify']['category'] = category.category_id
+    command_data['verify']['category'] = category.id
+    save_commands()
+
+    await interaction.followup.send("Category set.")
 
 
 client.run(token)
