7884d979b4f15f12b38c8549572ebdcfd636f1b1
hexmaker <61135254+Hexmaker543@users.noreply.github.com>
2026-05-28 03:36:07 -0400

    created absolute and relative input action rows

--- cogs/reminder.py
+++ cogs/reminder.py
@@ -2,6 +2,8 @@ import discord
 from discord import app_commands
 from discord.ext import commands
 
+from datetime import datetime
+
 
 
 class Reminder(commands.Cog):
@@ -28,6 +30,7 @@ class ReminderLayout(discord.ui.LayoutView):
         self.bot = bot
 
         self.time_type = discord.ui.Select(
+            custom_id='time_type_select',
             required=True,
             placeholder='Select One',
             options=[
@@ -40,18 +43,78 @@ class ReminderLayout(discord.ui.LayoutView):
             discord.ui.TextDisplay('Remind me '),
             discord.ui.ActionRow(self.time_type))
 
-    async def on_time_type_change(self):
+        self.submit_button = discord.ui.ActionRow(
+            discord.ui.Button(
+                custom_id='submit_reminder',
+                style=discord.ButtonStyle.success,
+                label='Set Reminder'))
+
+        self.absolute_inputs = discord.ui.ActionRow(
+            discord.ui.TextInput(
+                custom_id='month_select',
+                required=True,
+                placeholder='MM',
+                min_length=2,
+                max_length=2),
+            discord.ui.TextInput(
+                custom_id='day_select',
+                required=True,
+                placeholder='DD',
+                min_length=1,
+                max_length=2),
+            discord.ui.TextInput(
+                custom_id='year_select',
+                required=True,
+                placeholder='YYYY',
+                min_length=4,
+                max_length=4),
+            discord.ui.TextInput(
+                custom_id='time_select',
+                required=True,
+                placeholder='HHMM (Military Format)',
+                min_length=4,
+                max_length=4))
+
+        self.relative_inputs = discord.ui.ActionRow(
+            discord.ui.TextInput(
+                custom_id='years_select',
+                required=True,
+                placeholder='Years',
+                max_length=2),
+            discord.ui.TextInput(
+                custom_id='months_select',
+                required=True,
+                placeholder='Months',
+                max_length=2),
+            discord.ui.TextInput(
+                custom_id='days_select',
+                required=True,
+                placeholder='Days',
+                max_length=2),
+            discord.ui.TextInput(
+                custom_id='hours_select',
+                required=True,
+                placeholder='Hours',
+                max_length=2),
+            discord.ui.TextInput(
+                custom_id='minutes_select',
+                required=True,
+                placeholder='Minutes',
+                max_length=2))
+
+    async def on_time_type_change(self, interaction: discord.Interaction):
         items_to_remove = [item
                            for item in self.container.children[2:]]
         for item in items_to_remove:
             self.container.remove_item(item)
 
         if self.time_type.values[0] == 'on':
-            # Absolute
-            pass
-        else:
-            # Relative
-            pass
+            self.add_item(self.absolute_inputs)
+        elif self.time_type.values[0] == 'in':
+            self.add_item(self.relative_inputs)
+        else: 
+            return
+        self.add_item(self.submit_button)
 
 async def setup(bot: commands.Bot):
     await bot.add_cog(Reminder(bot))
